5784b82f00b8270b05b7740cfd53b2287891f4f4
Bapan Biswas <46675716+babiswas@users.noreply.github.com>
2024-10-29 13:30:34 +0530

    Update test12.sh
    commit

--- test12.sh
+++ test12.sh
@@ -2,5 +2,8 @@ https://stackoverflow.com/questions/63241009/aws-sts-assume-role-in-one-command
 aws configure list-profiles
 eval $(aws sts assume-role --role-arn arn:aws:iam::123456789123:role/myAwesomeRole --role-session-name test | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
 
-
-
+Account1    Account2
+                 Resource 
+                 Role to access the resource
+                 Policy attached to the role.
+<--------Trust----->
